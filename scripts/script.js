1d35e674b2ce93278be86659bc0ce5c7d0d2fb66
musa_dv <musa_dv@abv.bg>
2022-08-12 02:48:44 +0300

    view resume with one click

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const email = document.getElementById("email");
+const tooltip = document.getElementById("tooltiptext");
+
+email.addEventListener("click", copyEmailToClipboard);
+
+function copyEmailToClipboard() {
+  let range = document.createRange();
+  range.selectNode(document.getElementById("email"));
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges(); // to deselect
+  tooltip.innerText = "Copied :)";
+}
