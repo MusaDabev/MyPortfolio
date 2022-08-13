8558dd9a127b002d0f901e1bdc72b8226b030942
musa_dv <musa_dv@abv.bg>
2022-08-13 21:12:42 +0300

    add conatct section

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-60px";
+    document.getElementById("navbar").style.top = "-70px";
   }
   prevScrollpos = currentScrollPos;
 }
